0f61fb39a20c758b380505c24fc86a71571e4c35
jandestiny <39858598+jandestiny@users.noreply.github.com>
2020-11-17 16:28:03 +0100

    first push

---
+++ serviceworker.js
@@ -0,0 +1,40 @@
+self.addEventListener("fetch", (event) =>
+{
+    console.log("CAUGHT FETCH")
+
+    event.respondWith(
+        caches.match(event.request)
+            .then((result) =>
+            {
+                if (result) {
+                    console.log("found request in cache!", result)
+                    return result;
+                } else {
+                    console.log("did not find request in cache!", event.request)
+                    return fetch(event.request);
+                }
+            })
+    )
+
+})
+
+self.addEventListener("install", (event) =>
+{
+    console.log("service worker installed")
+
+    event.waitUntil(
+        caches.open("static")
+            .then((cacheObj) =>
+            {
+                cacheObj.addAll([
+                    "/",
+                    "/index.html",
+                    "/images/KlickiBunti-192x192.png"
+                ])
+            }))
+})
+
+self.addEventListener("activate", () =>
+{
+    console.log("service worker activated")
+})
